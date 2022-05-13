c59e666aff347aa1ff5a7c81d1f718f3cdbedf86
Sabrina <sabrina.ulmi@students.fhnw.ch>
2022-05-13 15:05:36 +0200

    Lora APRS is sending now:)

---
+++ src/Minimal_LoRa_APRS_Tracker.cpp
@@ -0,0 +1,77 @@
+#include <APRS-Decoder.h>
+#include <Arduino.h>
+#include <LoRa.h>
+
+#define LORA_SCK 18
+#define LORA_MISO 16
+#define LORA_MOSI 19
+#define LORA_CS 17
+#define LORA_RST 14
+#define LORA_IRQ 15
+
+void setup_lora();
+void setup_gps();
+
+// cppcheck-suppress unusedFunction
+void setup() {
+  Serial.begin(115200);
+
+  setup_lora();
+
+  Serial.println("setup done...");
+  delay(500);
+}
+
+// cppcheck-suppress unusedFunction
+void loop() {
+  delay(5000);
+
+  APRSMessage msg;
+  String      lat = "47.50979253564977";
+  String      lng = "7.620736862914847";
+  String      alt = "530";
+  msg.setSource("HEw45G");
+  msg.setPath("WIDE1-1");
+  msg.setDestination("APLT00"); 
+
+  String aprsmsg;
+  aprsmsg = "!" + lat + "/" + lng + "/" + alt;
+
+  msg.getAPRSBody()->setData(aprsmsg);
+  String data = msg.encode();
+  Serial.println(data);
+
+  LoRa.beginPacket();
+  // Header:
+  LoRa.write('<');
+  LoRa.write(0xFF);
+  LoRa.write(0x01);
+  // APRS Data:
+  LoRa.write((const uint8_t *)data.c_str(), data.length());
+  LoRa.endPacket();
+}
+
+
+void setup_lora() {
+  delay(10000);
+  Serial.println("Set SPI pins!");
+  SPI.begin();
+  Serial.println("Set LoRa pins!");
+  LoRa.setPins(LORA_CS, LORA_RST, LORA_IRQ);
+
+  long freq = 433775000;
+  Serial.print("frequency: ");
+  Serial.println(String(freq));
+  if (!LoRa.begin(freq)) {
+    Serial.println("Starting LoRa failed!");
+    while (true) {
+    }
+  }
+  LoRa.setSpreadingFactor(12);
+  LoRa.setSignalBandwidth(125000);
+  LoRa.setCodingRate4(5);
+  LoRa.enableCrc();
+
+  LoRa.setTxPower(20);
+  Serial.println("LoRa init done!");
+}
